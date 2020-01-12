f294cdcfa7bd0ee52315d85f81cf866a7b8bf196
Ares Meroueh <ares.m@faculty.ai>
2020-01-12 09:50:44 +0000

    Add data generation for neural_network_test

---
+++ test/neural_network/generate_data.py
@@ -0,0 +1,25 @@
+import sys 
+
+import numpy as np
+import tensorflow as tf
+
+save_dir = sys.argv[1]
+
+print(f"Using tensorflow version {tf.__version__}")
+
+graph = tf.Graph()
+
+input_data = np.array([[1.0, 0.0]], dtype=np.float32)
+
+with graph.as_default():
+    input = tf.placeholder(dtype=tf.float32, shape=None, name="input")
+    A = tf.constant([[1.0, -2.0], [3.0, 4.0]], dtype=tf.float32, name="A")
+    b = tf.constant([0.5, -0.5], dtype=tf.float32, name="b")
+    C = tf.matmul(input, A, name="C")
+    D = tf.add(C, b)
+    output = tf.tanh(D, name="output")
+
+tf.io.write_graph(graph, logdir=save_dir, name="graph.pb", as_text=False)
+
+with tf.Session(graph=graph) as session:
+    output_data = session.run(output, feed_dict={input: input_data})
