6879c4bd88120313fe34c06c88111713279dcbb2
Ares Meroueh <ares.meroueh@gmail.com>
2020-07-05 22:40:50 +0200

    Improve the python test script

--- test/python/test.py
+++ test/python/test.py
@@ -3,32 +3,37 @@ import numpy as np
 
 from az_connect_four.az_connect_four import ConnectFour, Model, Evaluator, Search, SearchPool
 
-N_THREADS = 16
+N_THREADS = 64
+N_SIMULATIONS_PER_MOVE = 200
+SEARCH_BATCH_SIZE = 8
+EVALUATOR_BATCH_SIZE = 64
+N_SEARCH_WORKERS = 8
+N_GAMES_PER_WORKER = 1000 // N_THREADS
 
 model = Model.create()
 model.load("model.pb", "value/sub", "policy/Softmax")
-evaluator = Evaluator(model, 4)
-pool = SearchPool(evaluator, 4)
+evaluator = Evaluator(model, EVALUATOR_BATCH_SIZE)
+pool = SearchPool(evaluator, N_SEARCH_WORKERS)
 
 def self_play(game, pool, evaluator):
-    while not game.finished():
-        search = Search(game, evaluator, 4, 800)
-        pool.perform_search(search)
-        root = search.get_tree_root()
-        move_counts = [0 for i in range(ConnectFour.get_policy_size())]
+    for _ in range(N_GAMES_PER_WORKER):
+        while not game.finished():
+            search = Search(game, evaluator, SEARCH_BATCH_SIZE, N_SIMULATIONS_PER_MOVE)
+            pool.perform_search(search)
+            root = search.get_tree_root()
+            move_counts = [0 for i in range(ConnectFour.get_policy_size())]
 
-        best_visit_count = -1
-        best_child = None
-        for i in range(root.get_n_children()):
-            child = root.get_child(i)
-            if child.get_n_visits() > best_visit_count:
-                best_visit_count = child.get_n_visits()
-                best_child = child
-       
-        move = best_child.get_move()
-        print(f"Playing move {move}")
-        game.play_move(move)
-    return game.score
+            best_visit_count = -1
+            best_child = None
+            for i in range(root.get_n_children()):
+                child = root.get_child(i)
+                if child.get_n_visits() > best_visit_count:
+                    best_visit_count = child.get_n_visits()
+                    best_child = child
+           
+            move = best_child.get_move()
+            print(f"Playing move {move}")
+            game.play_move(move)
 
 def worker(id, pool, evaluator):
     print(f"Thread {id} started")
