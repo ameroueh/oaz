d6267eb24304f22e83a9c9cd78cd2a43c5a44e0e
Ares Meroueh <ares.meroueh@gmail.com>
2020-11-14 10:17:09 +0100

    Add missing `oaz/utils/time.hpp`

---
+++ oaz/utils/time.hpp
@@ -0,0 +1,14 @@
+#ifndef __TIME_HPP__
+#define __TIME_HPP__
+
+#include <chrono>
+
+namespace oaz::utils {
+	
+	size_t time_now_ns() {
+		return std::chrono::duration_cast<std::chrono::nanoseconds>(
+			std::chrono::high_resolution_clock::now().time_since_epoch()
+		).count();
+	}
+}
+#endif
