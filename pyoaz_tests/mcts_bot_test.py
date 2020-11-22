61cd9be6696d11ecb1c5656e0bc275d5b6d4b2e1
snakch <simon.nakach@gmail.com>
2020-11-22 15:18:16 +0000

    black

--- pyoaz_tests/mcts_bot_test.py
+++ pyoaz_tests/mcts_bot_test.py
@@ -3,9 +3,6 @@ from pyoaz.bots.mcts_bot import MCTSBot
 
 
 def test_mcts_bot():
-    bot = MCTSBot(
-        n_iterations=100000,
-        n_concurrent_workers=2
-    )
+    bot = MCTSBot(n_iterations=100000, n_concurrent_workers=2)
     game = TicTacToe()
     bot.play(game)
