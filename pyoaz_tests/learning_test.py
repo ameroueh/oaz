9b0924c08f577ef6da3f0e525d1b0b6fb4f70fcb
Ares Meroueh <ares.meroueh@gmail.com>
2020-12-28 17:08:25 +0100

    Use fixture instead of global code

--- pyoaz_tests/learning_test.py
+++ pyoaz_tests/learning_test.py
@@ -5,6 +5,8 @@
 
 import logging
 import os
+import pytest
+import tempfile
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -17,107 +19,133 @@ from pyoaz.training import Trainer
 LOGGER = setup_logger()
 LOGGER.level = logging.INFO
 
-experiment_path = Path(__file__).absolute().parent / "resources"
-# save_path = experiment_path / "test_script"
+EXPERIMENT_PATH = Path(__file__).absolute().parent / "resources"
 
 SAVE_DIR = TemporaryDirectory()
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["OAZ_LOGGING"] = "false"
 
-CONFIGURATION = {
-    "game": "tic_tac_toe",
-    "model": {
-        "n_resnet_blocks": 3,
-        "activation": "tanh",
-        "optimizer": "adam",
-        "policy_factor": 1.0,
-    },
-    "save": {"save_path": SAVE_DIR.name, "checkpoint_every": 100,},
-    "benchmark": {
-        "tournament_frequency": 1,
-        "n_tournament_games": 10,
-        "n_best_self_games": 10,
-        "benchmark_path": experiment_path / "tic_tac_toe",
-        "mcts_bot_iterations": [10],
-    },
-    "self_play": {
-        "n_tree_workers": 4,
-        "n_threads": 8,
-        "n_workers": 4,
-        "evaluator_batch_size": 4,
-        "epsilon": 0.25,
-        "alpha": 1.0,
-    },
-    "stages": [
-        {
-            "n_generations": 2,
-            "learning_rate": 0.001,
-            "momentum": 0.9,
-            "buffer_length": 70000,
-            "n_purge": 0,
-            "n_simulations_per_move": 200,
-            "discount_factor": 0.99,
-            "n_games_per_worker": 256,
-            "update_epochs": 1,
-            "training_samples": 40000,
-        },
-        {
-            "n_generations": 3,
-            "learning_rate": 0.001,
-            "momentum": 0.9,
-            "buffer_length": 70000,
-            "n_purge": 35000,
-            "n_simulations_per_move": 400,
-            "discount_factor": 0.99,
-            "n_games_per_worker": 256,
-            "update_epochs": 1,
-            "training_samples": 40000,
-        },
-        {
-            "n_generations": 3,
-            "learning_rate": 0.001,
-            "momentum": 0.9,
-            "buffer_length": 150000,
-            "n_purge": 35000,
-            "n_simulations_per_move": 800,
-            "discount_factor": 1.0,
-            "n_games_per_worker": 256,
-            "update_epochs": 4,
-            "training_samples": 40000,
-        },
-    ],
-}
 
-trainer = Trainer(CONFIGURATION, logger=LOGGER)
-trainer.train()
-history = joblib.load(Path(SAVE_DIR.name) / "history.joblib")
+@pytest.fixture(scope="session")
+def train_model():
+    with tempfile.TemporaryDirectory() as save_dir:
+        save_dir = Path(save_dir)
+        configuration = {
+            "game": "tic_tac_toe",
+            "model": {
+                "n_resnet_blocks": 3,
+                "activation": "tanh",
+                "optimizer": "adam",
+                "policy_factor": 1.0,
+            },
+            "save": {
+                "save_path": save_dir.name,
+                "checkpoint_every": 100,
+            },
+            "benchmark": {
+                "tournament_frequency": 1,
+                "n_tournament_games": 10,
+                "n_best_self_games": 10,
+                "benchmark_path": EXPERIMENT_PATH / "tic_tac_toe",
+                "mcts_bot_iterations": [10],
+            },
+            "self_play": {
+                "n_tree_workers": 4,
+                "n_threads": 8,
+                "n_workers": 4,
+                "evaluator_batch_size": 4,
+                "epsilon": 0.25,
+                "alpha": 1.0,
+            },
+            "stages": [
+                {
+                    "n_generations": 2,
+                    "learning_rate": 0.001,
+                    "momentum": 0.9,
+                    "buffer_length": 70000,
+                    "n_purge": 0,
+                    "n_simulations_per_move": 200,
+                    "discount_factor": 0.99,
+                    "n_games_per_worker": 256,
+                    "update_epochs": 1,
+                    "training_samples": 40000,
+                },
+                {
+                    "n_generations": 3,
+                    "learning_rate": 0.001,
+                    "momentum": 0.9,
+                    "buffer_length": 70000,
+                    "n_purge": 35000,
+                    "n_simulations_per_move": 400,
+                    "discount_factor": 0.99,
+                    "n_games_per_worker": 256,
+                    "update_epochs": 1,
+                    "training_samples": 40000,
+                },
+                {
+                    "n_generations": 3,
+                    "learning_rate": 0.001,
+                    "momentum": 0.9,
+                    "buffer_length": 150000,
+                    "n_purge": 35000,
+                    "n_simulations_per_move": 800,
+                    "discount_factor": 1.0,
+                    "n_games_per_worker": 256,
+                    "update_epochs": 4,
+                    "training_samples": 40000,
+                },
+            ],
+        }
 
-n_generations = len(history["best_generation"])
-extremity_index = max(1, n_generations // 5)
+        trainer = Trainer(configuration, logger=LOGGER)
+        trainer.train()
+        history = joblib.load(Path(save_dir.name) / "history.joblib")
+
+        n_generations = len(history["best_generation"])
+        extremity_index = max(1, n_generations // 5)
+
+    return {
+        "history": history,
+        "n_generations": n_generations,
+        "extremity_index": extremity_index,
+    }
 
 
-def test_generation_improvement():
+def test_generation_improvement(train_model):
 
     # Check that the model improved a minimum number of times
+
+    history = train_model["history"]
+    n_generations = train_model["n_generations"]
+    extremity_index = train_model["extremity_index"]
+
     n_improvements = np.diff(history["best_generation"]).sum()
     min_improvements = min(n_generations // 4, 10)
     assert n_improvements > min_improvements
 
 
-def test_naive_bot_performance():
+def test_naive_bot_performance(train_model):
 
     # Check that the agent wins sufficiently against naive bots towards the end
+
+    history = train_model["history"]
+    n_generations = train_model["n_generations"]
+    extremity_index = train_model["extremity_index"]
+
     best_wins = max(history["wins"][-extremity_index:])
     assert best_wins > 35
 
 
-def test_mse_behaviour():
+def test_mse_behaviour(train_model):
+
     # Check that mse decreased and reached a certain threshold
+
+    history = train_model["history"]
+    n_generations = train_model["n_generations"]
+    extremity_index = train_model["extremity_index"]
+
     mses = history["mse"]
 
     assert np.mean(mses[:extremity_index]) > np.mean(mses[-extremity_index:])
     assert min(mses[-extremity_index:]) < 0.55
-
-
-SAVE_DIR.cleanup()
