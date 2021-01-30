6430b5af05c2d25b784f80fef0f38188352b8fec
snakch <simon.nakach@gmail.com>
2021-01-30 15:37:29 +0000

    Added an experiment with script

---
+++ experiments/starting_position_experiment/run_experiments.sh
@@ -0,0 +1,5 @@
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_1
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_2
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_3
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_4
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_5
