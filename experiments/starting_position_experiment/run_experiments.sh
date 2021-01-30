35e40b81797ce45b277efec73ebfafdb3a2b2ea7
snakch <simon.nakach@gmail.com>
2021-01-30 15:47:37 +0000

    Fixes for simpler experiments

--- experiments/starting_position_experiment/run_experiments.sh
+++ experiments/starting_position_experiment/run_experiments.sh
@@ -1,5 +1,5 @@
-python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_1
-python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_2
-python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_3
-python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_4
-python ../training_script.py --configuration_path starting_position_experiment.toml --save_path experiment_5
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path no_starting_positions/experiment_1
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path no_starting_positions/experiment_2
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path no_starting_positions/experiment_3
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path no_starting_positions/experiment_4
+python ../training_script.py --configuration_path starting_position_experiment.toml --save_path no_starting_positions/experiment_5
