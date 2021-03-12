ca410a35e2a28d9030000676c077cbf597aaa38a
Ares Meroueh <ares.meroueh@gmail.com>
2021-03-12 14:17:24 +0100

    Run black

--- pyoaz_tests/bots/az_bot_test.py
+++ pyoaz_tests/bots/az_bot_test.py
@@ -11,7 +11,10 @@ tf.disable_v2_behavior()
 def test_az_bot():
     model = create_tic_tac_toe_model()
 
-    bot = AZBot.from_keras_model(game_class=TicTacToe, model=model,)
+    bot = AZBot.from_keras_model(
+        game_class=TicTacToe,
+        model=model,
+    )
     bot.model.session.run(tf.global_variables_initializer())
 
     game = TicTacToe()
