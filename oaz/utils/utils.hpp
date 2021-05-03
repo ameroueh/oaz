d9d25a977bcb59ff305a37dc727c53deb6496cf7
Ares Meroueh <ares.meroueh@gmail.com>
2021-05-03 23:28:22 +0200

    Fix clang-tidy warnings in oaz/utils

--- oaz/utils/utils.hpp
+++ oaz/utils/utils.hpp
@@ -11,18 +11,21 @@ template <class Game>
 void loadBoardFromJson(const nlohmann::json& data,
                        typename Game::Board& board) {
   auto dimensions = Game::Board::Dimensions();
-  for (int i = 0; i != dimensions[0]; ++i)
-    for (int j = 0; j != dimensions[1]; ++j)
-      for (int k = 0; k != dimensions[2]; ++k) board[i][j][k] = data[i][j][k];
+  for (int i = 0; i != dimensions[0]; ++i) {
+    for (int j = 0; j != dimensions[1]; ++j) {
+      for (int k = 0; k != dimensions[2]; ++k) {board[i][j][k] = data[i][j][k];}
+    }
+  }
 }
 
 template <class ArrayRef, int N>
 struct RecursiveLoader {
   static void loadMultiArrayFromJson(const nlohmann::json& data,
                                      ArrayRef array) {
-    for (int i = 0; i != array.shape()[0]; ++i)
+    for (int i = 0; i != array.shape()[0]; ++i) {
       RecursiveLoader<typename ArrayRef::template subarray<N - 1>::type,
                       N - 1>::loadMultiArrayFromJson(data[i], array[i]);
+    }
   }
 };
 
@@ -44,7 +47,7 @@ void loadMultiArrayFromJson(const nlohmann::json& data, Array& array) {
 
 template <class Array>
 void loadArrayFromJson(const nlohmann::json& data, Array& array) {
-  for (size_t i = 0; i != array.size(); ++i) array[i] = data[i];
+  for (size_t i = 0; i != array.size(); ++i) {array[i] = data[i];}
 }
 
 bool CheckSearchTree(oaz::mcts::SearchNode* node) {
@@ -61,12 +64,14 @@ bool CheckSearchTree(oaz::mcts::SearchNode* node) {
       correct_children &= CheckSearchTree(child);
     }
 
-    if (!correct_children)
+    if (!correct_children) {
       std::cout << "Incorrect children at " << node << std::endl;
+    }
     bool correct = (n_visits == (n_children_visits + 1));
-    if (!correct)
+    if (!correct) {
       std::cout << "Incorrect node at " << node << "; n_children_visits "
                 << n_children_visits << "; n_visits " << n_visits << std::endl;
+    }
 
     overall_correct = correct && correct_children;
   }
